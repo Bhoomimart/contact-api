e1738e3b1fc6c34a9fcc019bb310e3198bccc35b
Himanshu72060 <himanshusharma00898@gmail.com>
2025-06-02 14:37:00 +0530

    first commit

--- models/Contact.js
+++ models/Contact.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
-    name: { type: String, required: true },
     email: { type: String, required: true },
+    phone: { type: String, required: true },
     message: { type: String, required: true },
 }, { timestamps: true });
 
